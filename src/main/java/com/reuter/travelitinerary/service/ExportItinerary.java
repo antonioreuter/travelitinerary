a37f99e0b04f218d23871cf654507b4b8da04532
Antonio Reuter <antonioreuter@gmail.com>
2016-04-08 00:31:13 -0300

    Added a comment into the ExportItinerary Service

--- src/main/java/com/reuter/travelitinerary/service/ExportItinerary.java
+++ src/main/java/com/reuter/travelitinerary/service/ExportItinerary.java
@@ -7,9 +7,18 @@ import java.nio.file.Path;
 import java.util.Collection;
 
 /**
+ * Service responsible for export the itinerary to a file
+ * <p>
  * Created by aandra1 on 07/04/16.
  */
 public interface ExportItinerary {
 
+  /**
+   * Exports the itinerary to a file.
+   *
+   * @param locations the locations of the itinerary
+   * @return the path where the itinerary file is located
+   * @throws IOException
+   */
   Path exportLocation(Collection<Location> locations) throws IOException;
 }
